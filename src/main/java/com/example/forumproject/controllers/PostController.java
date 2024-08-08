5a005c3d0e3dd34041d8bbece95da8d749aba4f2
Stefcho1227 <stefan.ivanov1227@gmail.com>
2024-08-08 15:05:10 +0300

    added SwaggerConfig class and like dependency

--- src/main/java/com/example/forumproject/controllers/PostController.java
+++ src/main/java/com/example/forumproject/controllers/PostController.java
@@ -14,6 +14,7 @@ import com.example.forumproject.models.dtos.in.PostDto;
 import com.example.forumproject.models.options.FilterOptions;
 import com.example.forumproject.services.contracts.PostService;
 import com.example.forumproject.services.contracts.TagService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -32,6 +33,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/posts")
+@SecurityRequirement(name = "basicAuth")
 public class PostController {
     private final PostService postService;
     private final TagService tagService;
