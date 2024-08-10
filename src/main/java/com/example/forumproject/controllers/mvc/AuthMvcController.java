864c8850a3281d16ade9fa8af69523c4c47c8330
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-08-10 17:43:34 +0300

    made the sign in button on the navbar work and added a very basic auth controller also refactorred some directories

---
+++ src/main/java/com/example/forumproject/controllers/mvc/AuthMvcController.java
@@ -0,0 +1,14 @@
+package com.example.forumproject.controllers.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthMvcController {
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "LoginView";
+    }
+}
